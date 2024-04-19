c59767792c9bcccdc09fa162effbb8b9d84f9d48
Sergio Camilo Madero Baldión <smadero@wi-mobile.com>
2024-04-19 10:47:51 -0500

    Creacion inicial

---
+++ GeneradorPaginas/src/main/java/cca/generadorpaginas/GeneradorPaginas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cca.generadorpaginas;
+
+/**
+ *
+ * @author smadero
+ */
+public class GeneradorPaginas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
