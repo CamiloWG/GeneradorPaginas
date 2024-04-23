c4b860d57bfa99667289bf4dce6a75678c7cb1dd
Camilo Madero <smadero@wi-mobile.com>
2024-04-22 21:31:21 -0500

    Re-do generar contenido base de HTML

--- GeneradorPaginas/src/main/java/Generator/Generador.java
+++ GeneradorPaginas/src/main/java/Generator/Generador.java
@@ -4,10 +4,26 @@
  */
 package Generator;
 
+import java.util.Map;
+
 /**
  *
  * @author Camilo & Paula
  */
 public class Generador {
     
+    private Map<String, Map<String, String>> terminales;
+    private String contentGenerated;
+    
+    public Generador(Map<String, Map<String, String>> terminales) {
+        this.terminales = terminales;
+    }
+    
+    
+    private void generateBase() {
+        String T = terminales.get("pagina") != null ? terminales.get("pagina").get("contenido") : "Sin Titulo";
+        String Q = "<head><title>"+ T +"</title></head><body>"+ B +"</body>";
+        String H = "<html>"+ Q +"</html>";
+        String P = "<!DOCTYPE html>" + H;
+    }
 }
